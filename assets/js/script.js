85565f8d47b8184526e53876c80e42555eee3a6d
Gospel Chukwu <brondchux@gmail.com>
2021-07-28 09:17:13 -0400

    Successfully stored todo and retrieved todos from localstorage and made them persistent as the textarea value, works fine

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,10 +21,10 @@ function createTimeBlock(theTime) {
 	let timeBlockHour = $("<span class='hour-area'></span>");
 	let timeBlockTextareaCol = $("<div class='col-10 past px-0'>");
 	let timeBlockTextarea = $(
-		"<textarea name='' class='task-area description todoBlock' id='' rows='4'></textarea>"
+		"<textarea name='' class='task-area description todo-block' id='' rows='4'></textarea>"
 	);
 	let timeBlockSaveCol = $("<div class='col-1 save-btn d-flex'>");
-	let timeBlockSave = $("<i class='fas fa-save m-auto fa-lg'></i>");
+	let timeBlockSave = $("<i class='fas fa-save m-auto fa-lg save-button'></i>");
 
 	// Add classes based on current hour
 
@@ -41,35 +41,49 @@ function createTimeBlock(theTime) {
 	}
 
 	// Build
-	timeBlockHourCol = timeBlockHourCol
-		.append(timeBlockHour)
-		.text(`${theTime > 11 ? `${theTime} PM` : `${theTime} AM`}`);
+	let timeText = `${theTime > 11 ? `${theTime} PM` : `${theTime} AM`}`;
+	timeBlockHourCol = timeBlockHourCol.append(timeBlockHour).text(timeText);
 	timeBlockTextareaCol = timeBlockTextareaCol.append(timeBlockTextarea);
 	timeBlockSaveCol = timeBlockSaveCol.append(timeBlockSave);
+
+	// Retrieve todos
+	timeBlockTextarea.val(fetchTodo(timeText));
 
 	// Place
 	timeBlockRow.append(timeBlockHourCol);
 	timeBlockRow.append(timeBlockTextareaCol);
 	timeBlockRow.append(timeBlockSaveCol);
 	$(".container").append(timeBlockRow);
-
-	// Add action to save btn
-	timeBlockSave.on("click", saveTask);
 }
 
 // Display work hours
 function workingHours() {
+	// create time blocks
 	for (let i = 9; i <= 17; i++) {
 		createTimeBlock(i);
 	}
+
+	// listen for clicks on save buttons
+	$(".save-button").on("click", saveTask);
 }
 
 // Save task to local storage
 function saveTask() {
-	let textAreaVal = $(".todoBlock").val();
-	let hourVal = $(".hour-area").text();
-	console.log("you typed: ", textAreaVal, " at ", hourVal);
+	let textAreaVal = $(this).parent().parent().find(".todo-block").val();
+	let hourVal = $(this).parent().parent().find(".hour").text();
+
+	// Store in localstorage
+	localStorage.setItem(`${hourVal}`, `${textAreaVal}`);
 }
+
+// Fetch stored todo
+function fetchTodo(hour) {
+	// Update the textarea
+	let storedTextAreaVal = localStorage.getItem(`${hour}`);
+	return `${storedTextAreaVal ? storedTextAreaVal : ""}`;
+}
+
+// Event deligation example
 
 // INITIALIZATION ==========================================
 displayCurrentDay();
